cdf5ebeb0ab0185c5aa3769077e68326f0b569ff
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-08-08 20:15:24 +0530

    Done Branch entity👍.

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/entity/Branch.java
@@ -0,0 +1,29 @@
+package com.cpd.hotel_system.hotel_management_service_api.entity;
+
+import com.cpd.hotel_system.hotel_management_service_api.enums.BranchType;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name="branch")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Branch {
+    @Id
+    @Column(name = "branch_id", length=80)
+    private String branchId;
+
+    @Column(name = "room_count")
+    private int roomCount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "branch_type", nullable = false)
+    private BranchType branchType;
+
+    @Column(name = "branch_name", nullable = false)
+    private String branchName;
+
+}
