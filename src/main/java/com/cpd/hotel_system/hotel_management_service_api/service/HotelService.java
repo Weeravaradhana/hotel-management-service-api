4aa415a06efc17c25b6e61522c668b8834ff0a77
Weeravaradhana <thaminduweeravardana@gmail.com>
2025-08-09 12:05:27 +0530

    Done implement service interfaces👍.

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/service/HotelService.java
@@ -0,0 +1,15 @@
+package com.cpd.hotel_system.hotel_management_service_api.service;
+
+import com.cpd.hotel_system.hotel_management_service_api.dto.request.RequestHotelDto;
+import com.cpd.hotel_system.hotel_management_service_api.dto.response.ResponseHotelDto;
+import com.cpd.hotel_system.hotel_management_service_api.dto.response.paginate.HotelPaginateResponseDto;
+
+import java.sql.SQLException;
+
+public interface HotelService {
+    public void create(RequestHotelDto dto);
+    public void update(RequestHotelDto dto, String hotelId) throws SQLException;
+    public void delete(String hotelId);
+    public ResponseHotelDto findById(String hotelId) throws SQLException;
+    public HotelPaginateResponseDto findAll(int page, int size, String searchText);
+}
